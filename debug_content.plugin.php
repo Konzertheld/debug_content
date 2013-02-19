04fa56d11210b176ff6ea67ae6c0075b76c0d3c3
Konzertheld <christian.gredig@gmail.com>
2013-02-19 01:50:50 +0100

    Simple functionality to automatically create a number of posts

--- debug_content.plugin.php
+++ debug_content.plugin.php
@@ -12,5 +12,34 @@ class DebugContent extends Plugin
 	{
 		Stack::add('template_header_javascript', $this->get_url(true) . 'jsdump.js', 'jsdump');
 	}
+	
+	public function configure()
+	{
+		$form = new FormUI(__CLASS__);
+		$form->append('text', 'postnumber', 'null:null', 'Number of posts to create per content type');
+		$form->append('submit', 'submit', 'Do it!');
+		$form->on_success(array($this,'create_posts'));
+		$form->out();
+	}
+	
+	public function create_posts($form)
+	{
+		if(!isset($form->postnumber)) return;
+		$post_types = Post::list_active_post_types();
+		foreach($post_types as $name => $number)
+		{
+			if(0 === $number) continue;
+			for($i=0;$i<$form->postnumber->value;$i++)
+			{
+				$post = new Post();
+				$post->user_id = User::identify()->id;
+				$post->title = "$name test entry";
+				$post->content = "Random content to insert here";
+				$post->pubdate = DateTime::date_create();
+				$post->insert();
+				$post->publish();
+			}
+		}
+	}
 }
 ?>
